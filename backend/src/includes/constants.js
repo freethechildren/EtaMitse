04acf906fe46ef3389a85e5ebf6fd0cb60c31c0d
Shazz Amin <shazz.amin@edu.uwaterloo.ca>
2018-07-02 01:22:15 -0400

    Added support for environment variables

--- backend/src/includes/constants.js
+++ backend/src/includes/constants.js
@@ -1,4 +1,9 @@
+const { getEnvironmentVariables } = require("./utilities");
+
 module.exports = {
+  ...getEnvironmentVariables([
+    "NETWORK_PORT",
+  ]),
   MEETING_CODE_DIGITS: 4,
   POSSIBLE_ESTIMATES: [0.5, 1, 2, 3, 5, 8, 13],
 };
--- frontend/src/includes/constants.js
+++ frontend/src/includes/constants.js
@@ -1,6 +1,10 @@
-const DEV = true;
+import utilities from "./utilities";
+
+const { getEnvironmentVariables } = utilities;
 
 export default {
-  SERVER_URL: DEV ? "ws://127.0.0.1:8080" : "wss://etamitse.herokuapp.com",
+  ...getEnvironmentVariables([
+    "SERVER_URL",
+  ]),
   POSSIBLE_ESTIMATES: [0.5, 1, 2, 3, 5, 8, 13],
 };
